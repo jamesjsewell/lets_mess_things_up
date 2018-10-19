c8a10c8b0a3087c01ec55d4e42a2ba3bfde82c9e
james sewell <jamesjosephsewell@gmail.com>
2018-10-19 10:34:49 -0500

    james made a change to the main.js and style sheet

--- main.js
+++ main.js
@@ -3,13 +3,12 @@ let pageDiv = document.getElementById('pageDiv')
 let mainHeading = document.getElementById('heading')
 
 buttonChanger.addEventListener('click', () => {
-    if (pageDiv.attributes.class.value == 'primary_background') {
-        mainHeading.innerHTML = 'Hello Destruction!'
-        pageDiv.setAttribute('class', 'secondary_background')
-        console.log("COMMMMMEEE ONNN")
-    } else {
-        console.log("leeeettttt")
-        mainHeading.innerHTML = 'Hello World!'
-        pageDiv.setAttribute('class', 'primary_background')
-    }
+  if (pageDiv.attributes.class.value === 'primary_background') {
+    mainHeading.innerHTML = 'Hello Destruction!'
+    pageDiv.setAttribute('class', 'secondary_background')
+  } else {
+    mainHeading.innerHTML = 'Aloha Planet!'
+    mainHeading.className = 'mainHeadingText'
+    pageDiv.setAttribute('class', 'primary_background')
+  }
 })
